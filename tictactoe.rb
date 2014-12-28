1cd8085c97847e9023b7057b6ca0244f056c96aa
Andrew Antle <andrew.antle@gmail.com>
2014-12-28 18:57:45 -0500

    No abbreviations

--- tictactoe.rb
+++ tictactoe.rb
@@ -16,40 +16,40 @@ puts "Starting tic-tac-toe..."
 players = ['X', 'O']
 current_player = players[rand(2)]
 
-# Create an instance of the Board class and assign it to b
-b = Board.new(current_player)
+# Create an instance of the Board class
+board = Board.new(current_player)
 
 # Display the initial blank board
 # (1. Draw the current board)
-b.display()
+board.display()
 puts
 
 
 # The main game loop
 # (Until the board is full AND there is no winner)
 #  While the board isn't full AND there is no winner
-while not b.board_full() and not b.winner()
+while not board.board_full() and not board.winner()
 
   # Prompt the current player for a move
   # (2. Ask the player for a move)
-  b.ask_player_for_move(current_player)
+  board.ask_player_for_move(current_player)
 
   # Get the next player
   # (3. Make sure the move is valid), (4. Store the current move)
-  current_player = b.get_next_turn()
+  current_player = board.get_next_turn()
 
   # (1. Draw the current board)
-  b.display()
+  board.display()
   puts
 
 end
 
 
 # Is there a winner?
-if b.winner()
+if board.winner()
 
   # Yes, print the winner's name
-  puts "Player " + b.get_next_turn() + " wins."
+  puts "Player " + board.get_next_turn() + " wins."
 
 else
 
@@ -59,4 +59,4 @@ else
 end
 
 # Wah wah wahhh
-puts "Game Over."
+puts "Game Over!"
